794df92ac373fdd6140e93a6d7e85a303a472760
Balabrahmam07 <136158779+Balabrahmam07@users.noreply.github.com>
2026-09-23 14:46:49 +0530

    ✅ Second Largest Digit in a String | Easy | Python | Runtime: 3 ms (73.89%) | Memory: 19.2 MB | 2026-09-23

--- leetcode/easy/1796-second-largest-digit-in-a-string/solution.py
+++ leetcode/easy/1796-second-largest-digit-in-a-string/solution.py
@@ -1,14 +1,14 @@
 class Solution:
     def secondHighest(self, s: str) -> int:
-        largest = 0
-        second = 0
+        largest = -1
+        second = -1
         for i in s:
             if not i.isalpha():
                 n = int(i)
                 if n > largest:
                     second = largest
                     largest = n
-                elif largest == second:
-                    second = -1
+                elif largest > n > second:
+                    second = n
                 
         return second
