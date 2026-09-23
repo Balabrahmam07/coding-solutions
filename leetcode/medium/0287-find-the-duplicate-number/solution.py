c7f226ef84775cea2de9da403a27ecf81cbf2135
Balabrahmam07 <136158779+Balabrahmam07@users.noreply.github.com>
2026-09-23 17:20:24 +0530

    ✅ Find the Duplicate Number | Medium | Python | Runtime: 0 ms | Memory: 19.3 MB | 2026-09-23

--- leetcode/medium/0287-find-the-duplicate-number/solution.py
+++ leetcode/medium/0287-find-the-duplicate-number/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def findDuplicate(self, nums: list[int]) -> int:
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return num
-            seen.add(num)
+        seen = nums[0]
+        for num in range(1, len(nums)):
+            if nums[num] == seen:
+                return nums[num]
+            seen = (max(seen, nums[num]) - min(seen, nums[num])) + 1
