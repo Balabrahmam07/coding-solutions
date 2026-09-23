802000c25cdbbf9be8cd03ef14a4acff2ac93108
Balabrahmam07 <136158779+Balabrahmam07@users.noreply.github.com>
2026-09-23 14:05:51 +0530

    ✅ Majority Element | Easy | Python | Runtime: 7 ms (61.80%) | Memory: 21.5 MB | 2026-09-23

--- leetcode/easy/0169-majority-element/solution.py
+++ leetcode/easy/0169-majority-element/solution.py
@@ -2,4 +2,10 @@ from collections import Counter
 
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
-        return Counter(nums).most_common(1)[0][0]
+        candidate = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+        return candidate
